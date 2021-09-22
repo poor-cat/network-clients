1abab803b95c7dffcddd79d9a0e0aa0fd8d87c07
A Poor Cat <82797313+poor-cat@users.noreply.github.com>
2021-09-22 09:10:21 +0530

    Update http-connect.py

--- http-connect.py
+++ http-connect.py
@@ -4,4 +4,4 @@ connection = http.client.HTTPConnection('www.domain.xyz', 80, timeout=10)
 
 print(connection)
 
-#simple script to connect to specific domain name and port in python
+#simple script to connect to specific domain name and port in python :)
