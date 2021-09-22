aa20716a7e65f13ac2199e4e079efd9bb6b8643d
A Poor Cat <82797313+poor-cat@users.noreply.github.com>
2021-09-22 09:20:49 +0530

    Create Get_request.py

---
+++ Get_request.py
@@ -0,0 +1,14 @@
+#sending Get request to a domain and fetching a data in specific format
+
+import http.client
+
+connection = http.client.HTTPSConnection("www.journaldev.com")
+
+connection.request("GET", "/")
+
+response = connection.getresponse()
+
+
+print("Status: {} and reason: {}".format(response.status, response.reason))
+
+connection.close()
